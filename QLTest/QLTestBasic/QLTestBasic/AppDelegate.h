1cd367970641b7521afd934cc48571cfd9f9793e
Shreker <gwinabc@foxmail.com>
2016-03-22 14:10:59 +0800

    Import Source

---
+++ QLTest/QLTestBasic/QLTestBasic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QLTestBasic
+//
+//  Created by Shrek on 16/3/22.
+//  Copyright © 2016年 Shreker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QLTest/QLTestBasic/QLTestBasic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QLTestBasic
+//
+//  Created by Shrek on 16/3/22.
+//  Copyright © 2016年 Shreker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
