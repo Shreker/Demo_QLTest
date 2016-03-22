ce8a9d8520d80a6deed31bf5199944008d9cfe18
Shreker <gwinabc@foxmail.com>
2016-03-22 14:21:27 +0800

    Add Notes of methods.

--- QLTest/QLTestBasic/QLTestBasicTests/QLTestBasicTests.m
+++ QLTest/QLTestBasic/QLTestBasicTests/QLTestBasicTests.m
@@ -6,6 +6,12 @@
 //  Copyright © 2016年 Shreker. All rights reserved.
 //
 
+/*!
+ * XCTest中所有的测试用例的命名都是以test开头的
+ * Setup是在所有测试用例运行之前运行的函数，在这个测试用例里进行一些通用的初始化工作
+ * tearDown是在所有的测试用例都执行完毕后执行的
+ */
+
 #import <XCTest/XCTest.h>
 
 @interface QLTestBasicTests : XCTestCase
